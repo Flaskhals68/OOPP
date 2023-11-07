4bb4e5e6e726ff070faecf6fee306d35d09eff02
BooloCaptain <112878577+BooloCaptain@users.noreply.github.com>
2023-11-07 20:20:04 +0100

    Restructure project to Maven
    Restructure project to a Maven project to enable building and testing.

--- src/main/java/com/group4/app/App.java
+++ src/main/java/com/group4/app/App.java
@@ -1,10 +1,12 @@
-import controller.HudController;
-import controller.InventoryController;
-import controller.WorldController;
-import model.Model;
-import view.HudView;
-import view.InventoryView;
-import view.WorldView;
+package com.group4.app;
+
+import com.group4.app.controller.HudController;
+import com.group4.app.controller.InventoryController;
+import com.group4.app.controller.WorldController;
+import com.group4.app.model.Model;
+import com.group4.app.view.HudView;
+import com.group4.app.view.InventoryView;
+import com.group4.app.view.WorldView;
 
 public class App {
     public static void main(String[] args) {
