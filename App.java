676b6f7ac41afe5f0918bd65e3f3692537e94f3b
erbergli <erik.berglind@gmail.com>
2023-11-07 08:15:23 +0100

    Draw game frame

--- App.java
+++ App.java
@@ -2,12 +2,14 @@ import controller.HudController;
 import controller.InventoryController;
 import controller.WorldController;
 import model.Model;
+import view.GameWindow;
 import view.HudView;
 import view.InventoryView;
 import view.WorldView;
 
 public class App {
     public static void main(String[] args) {
+        GameWindow gw = GameWindow.getInstance();
         Model model = new Model();
 
         WorldController worldController = new WorldController();
--- view/GameWindow.java
+++ view/GameWindow.java
@@ -1,10 +1,36 @@
 package view;
 
-public class GameWindow {
-    private static GameWindow instance = null;
-    private GameWindow() {}
+import javax.swing.*;
+import javax.swing.JFrame;
+import java.awt.*;
 
-    public GameWindow getInstance() {
+/*
+ * This is the frame where every other panel is drawn on. 
+ * Should draw each view. 
+ */
+public class GameWindow extends JFrame{
+    private static GameWindow instance = null;
+    private HudView hudView;
+    private InventoryView inventoryView;
+    private WorldView worldView;
+
+    private GameWindow() {
+        initComponents();
+    }
+
+    //TODO should make so that each view gets added to the frame
+    private void initComponents (){
+        setTitle("GAME");
+        setPreferredSize(new Dimension(1280,720));
+        setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation((dim.width/2-this.getSize().width/2), (dim.height/2-this.getSize().height/2));
+    }
+
+    public static GameWindow getInstance() {
         if (instance == null) {
             return new GameWindow();
         }
@@ -12,4 +38,6 @@ public class GameWindow {
             return GameWindow.instance;
         }
     }
+
+
 }
