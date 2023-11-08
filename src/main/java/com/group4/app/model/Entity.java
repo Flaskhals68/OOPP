c5049af61017343ade50572928f0abd7bb4c8e3c
Flaskhals68 <03kajo@gmail.com>
2023-11-08 14:12:58 +0100

    Add Entity class

---
+++ src/main/java/com/group4/app/model/Entity.java
@@ -0,0 +1,32 @@
+package com.group4.app.model;
+
+public class Entity {
+    private Tile tile;
+    private World world;
+
+    public Entity() {
+        this(null);
+    }
+
+    public Entity(Tile tile) {
+        this.tile = tile;
+    }
+
+    public int getXPos() {
+        return tile.getXPos();
+    }
+
+    public int getYPos() {
+        return tile.getYPos();
+    }
+
+    /**
+     * Returns a copy of the entity's tile
+     * @return
+     */
+    public Tile getTile() {
+        return new Tile(this.world, getXPos(), getYPos());
+    }
+    
+    
+}
---
+++ src/test/java/com/group4/app/Test.java
@@ -0,0 +1,5 @@
+package com.group4.app;
+
+public @interface Test {
+
+}
