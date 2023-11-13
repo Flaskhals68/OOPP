c792b12dadf1f54fae8433a8ac3f681cce57854a
AstridEmerath <astrid.emerath@gmail.com>
2023-11-13 15:08:34 +0100

    write inventory tests

---
+++ src/test/java/com/group4/app/testmodel/TestInventory.java
@@ -0,0 +1,48 @@
+package com.group4.app.testmodel;
+
+import com.group4.app.model.Inventory;
+import com.group4.app.model.Weapon;
+import com.group4.app.model.WeaponFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestInventory {
+    Inventory inv;
+    Weapon weapon1, weapon2;
+    @BeforeEach
+    void init() {
+        inv = new Inventory();
+        weapon1 = WeaponFactory.createSword();
+        weapon2 = WeaponFactory.createSword();
+    }
+    @Test
+    public void testAddItemIfItDoesNotExistYet() {
+        inv.addItem(weapon1);
+        assertEquals(weapon1.getName(), inv.getItem(weapon1.getName()).getName());
+    }
+
+    @Test
+    public void testAddItemIfItDoesExist() {
+
+        inv.addItem(weapon1);
+        inv.addItem(weapon2);
+
+        assertEquals(weapon1.getName(), inv.getItem(weapon1.getName()).getName());
+        assertEquals(weapon2.getName(), inv.getItem(weapon2.getName()).getName());
+    }
+
+    @Test
+    public void testGetItemIfDoesNotExist() {
+        inv.addItem(weapon1);
+        inv.getItem(weapon1.getName());
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            inv.getItem(weapon1.getName());
+        }, "IllegalArgumentException was expected");
+
+    }
+}
