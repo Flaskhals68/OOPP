a20a528552fed0c198b3af921e29f3868db27e19
Flaskhals68 <03kajo@gmail.com>
2023-11-20 13:57:19 +0100

    Implement IGameView interface

--- src/main/java/com/group4/app/view/HudView.java
+++ src/main/java/com/group4/app/view/HudView.java
@@ -2,15 +2,37 @@ package com.group4.app.view;
 
 import javax.swing.JPanel;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import com.group4.app.controller.HudController;
 import com.group4.app.model.Model;
 
-public class HudView extends JPanel{
+public class HudView extends JPanel implements IGameView {
     private Model model;
     private HudController controller;
+
+    private GridBagConstraints btnConstraints = new GridBagConstraints();
 
     public HudView(Model model, HudController controller) {
         this.model = model;
         this.controller = controller;
     }
+
+    private void initComponents() {
+        
+    }
+
+    @Override
+    public void updateView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateView'");
+    }
+
+    @Override
+    public JPanel getView() {
+        return this;
+    }
+
+
 }
