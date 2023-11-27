1203807d80e1be046d75b7501c2dacc695cd0037
AstridEmerath <astrid.emerath@gmail.com>
2023-11-27 15:54:03 +0100

    start writing testcreature

--- src/test/java/com/group4/app/testmodel/TestCreature.java
+++ src/test/java/com/group4/app/testmodel/TestCreature.java
@@ -1,12 +1,32 @@
 package com.group4.app.testmodel;
 
-import com.group4.app.model.Player;
-import com.group4.app.model.WeaponFactory;
+import com.group4.app.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestCreature {
 
+    private class ConcreteCreature extends Creature {
+
+        public ConcreteCreature(String id, String floorId, int xPos, int yPos, int ap, Weapon weapon, Attributes attr, int level) {
+            super(id, floorId, xPos, yPos, ap, weapon, attr, level);
+        }
+
+        @Override
+        public void death() {
+
+        }
+
+        @Override
+        public void startTurn() {
+
+        }
+
+        @Override
+        public void endTurn() {
+
+        }
+    }
     @BeforeEach
     public void setup() {
 
