30119c8607740124646f0d4b2d0d88304410eff0
AstridEmerath <astrid.emerath@gmail.com>
2023-11-29 16:39:00 +0100

    wrote healthpotion test

---
+++ src/test/java/com/group4/app/testmodel/TestPotion.java
@@ -0,0 +1,23 @@
+package com.group4.app.testmodel;
+
+import com.group4.app.model.Model;
+import com.group4.app.model.Player;
+import com.group4.app.model.Potion;
+import com.group4.app.model.PotionFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPotion {
+    @Test
+    public void testUseHealthPotion() {
+        Model.getInstance().addBasicMap(10, 0);
+        Player player = Model.getInstance().getPlayer();
+        Potion hpPot = PotionFactory.createHealthPotion();
+        int maxHP = player.getHitPoints();
+
+        player.takeHit(8);
+        hpPot.use(player);
+        assertEquals(maxHP, player.getHitPoints());
+    }
+}
