0f11cf86372a68935ee1ef6b065854b00d878f7c
erbergli <erik.berglind@gmail.com>
2023-12-08 13:37:18 +0100

    Small update in Model

--- src/main/java/com/group4/app/model/Model.java
+++ src/main/java/com/group4/app/model/Model.java
@@ -160,15 +160,12 @@ public class Model implements IWorldContainer {
     }
 
     public void startPlayerTurn(){
-        //TODO maybe dont access controller?
-        // StateController.setState(ActionState.IDLE);
         System.out.println("Player turn started");
         this.isPlayerTurn = true;
         updateObservers();
     }
 
     public void endPlayerTurn(){
-        // StateController.setState(ActionState.DISABLED);
         System.out.println("Player turn ended");
         this.isPlayerTurn = false;
         updateObservers();
