162d1fd1ff2142f84cb5dca060ff911da156ff54
erbergli <erik.berglind@gmail.com>
2023-12-09 23:03:26 +0100

    remove color array

--- src/main/java/com/group4/app/view/worldView/EntityPanelGenerator.java
+++ src/main/java/com/group4/app/view/worldView/EntityPanelGenerator.java
@@ -21,7 +21,6 @@ import javax.swing.JPanel;
 public class EntityPanelGenerator{
     private Map<String, JPanel> entityPanelMap = new HashMap<>();
     private String[] idArray = {"stone", "Zombie", "Skeleton", "player","deadEnemy", "playerDead"};
-    private Color[] colorArray = {Color.yellow, Color.gray, Color.green, Color.lightGray, Color.blue};
     private String[] imagePathArray = {"src/resources/stone_tile.png", "src/resources/ghoul.png", "src/resources/skeletal_warrior.png", "src/resources/player.png","src/resources/dead_enemy_stone.png","src/resources/dead_player_stone.png"};
     private int tile_width;
     private int tile_height;
