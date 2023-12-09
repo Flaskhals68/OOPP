ea7c052defc528d8b8b4539753259e9a3f33c7b1
erbergli <erik.berglind@gmail.com>
2023-12-09 23:04:44 +0100

    Fix code layout

--- src/main/java/com/group4/app/view/worldView/EntityPanelGenerator.java
+++ src/main/java/com/group4/app/view/worldView/EntityPanelGenerator.java
@@ -21,7 +21,14 @@ import javax.swing.JPanel;
 public class EntityPanelGenerator{
     private Map<String, JPanel> entityPanelMap = new HashMap<>();
     private String[] idArray = {"stone", "Zombie", "Skeleton", "player","deadEnemy", "playerDead"};
-    private String[] imagePathArray = {"src/resources/stone_tile.png", "src/resources/ghoul.png", "src/resources/skeletal_warrior.png", "src/resources/player.png","src/resources/dead_enemy_stone.png","src/resources/dead_player_stone.png"};
+    private String[] imagePathArray = {
+        "src/resources/stone_tile.png",
+        "src/resources/ghoul.png",
+        "src/resources/skeletal_warrior.png",
+        "src/resources/player.png",
+        "src/resources/dead_enemy_stone.png",
+        "src/resources/dead_player_stone.png"
+    };
     private int tile_width;
     private int tile_height;
 
