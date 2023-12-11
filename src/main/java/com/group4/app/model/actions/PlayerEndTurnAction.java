b23caf04c19e8817e0a0e2442b54f9d8872ffb74
erbergli <erik.berglind@gmail.com>
2023-12-11 13:35:54 +0100

    Implement unused methods in PlayerEndTurnAction

--- src/main/java/com/group4/app/model/actions/PlayerEndTurnAction.java
+++ src/main/java/com/group4/app/model/actions/PlayerEndTurnAction.java
@@ -1,8 +1,10 @@
 package com.group4.app.model.actions;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import com.group4.app.model.ITurnTaker;
+import com.group4.app.model.TurnHandler;
 import com.group4.app.model.creatures.Creature;
 import com.group4.app.model.creatures.Player;
 
@@ -14,14 +16,14 @@ public class PlayerEndTurnAction extends Action<ITurnTaker, ITurnTaker>{
 
     @Override
     public Set getTargetable() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTargetable'");
+        Set<ITurnTaker> tts = new HashSet<>();
+        tts.add(getActionTaker());
+        return tts;
     }
 
     @Override
     public Set getTargetablePositions() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTargetablePositions'");
+        return getTargetable();
     }
 
 
