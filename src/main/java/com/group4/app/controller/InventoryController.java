80fc6cce16ebf7a50675d72b055861a37d2cdb48
AstridEmerath <astrid.emerath@gmail.com>
2023-12-11 14:39:01 +0100

    start work on inventory view

--- src/main/java/com/group4/app/controller/InventoryController.java
+++ src/main/java/com/group4/app/controller/InventoryController.java
@@ -1,5 +1,11 @@
 package com.group4.app.controller;
 
+import com.group4.app.model.Model;
+import com.group4.app.model.creatures.Player;
+
 public class InventoryController {
-    
+    public void useItem(String itemName) {
+        Player p = Model.getInstance().getPlayer();
+        p.useItemFromInventory(itemName);
+    }
 }
--- src/main/java/com/group4/app/view/InventoryView.java
+++ src/main/java/com/group4/app/view/InventoryView.java
@@ -1,16 +1,82 @@
 package com.group4.app.view;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import com.group4.app.controller.InventoryController;
 import com.group4.app.model.Model;
 
-public class InventoryView extends JPanel{
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Map;
+
+public class InventoryView extends SidePanel implements IGameView{
     private Model model;
     private InventoryController controller;
 
     public InventoryView(Model model, InventoryController controller) {
         this.model = model;
         this.controller = controller;
+        initComponents();
+    }
+
+    private void initComponents() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        initInventory();
+    }
+
+    private void initInventory() {
+        removeAll();
+        Map<String, Integer> inventory = model.getPlayer().getInventoryItems();
+
+        for(Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            JPanel jPan = new JPanel();
+            jPan.setPreferredSize(new Dimension(100, 25));
+            jPan.add(new JLabel(entry.getKey() + ": " + entry.getValue()));
+            jPan.setBackground(Color.GRAY);
+            jPan.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+            jPan.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    controller.useItem(entry.getKey());
+                    initInventory();
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+            add(jPan);
+        }
+
+        revalidate();
+        repaint();
+    }
+
+    @Override
+    public void updateView() {
+        initInventory();
+    }
+
+    @Override
+    public JPanel getView() {
+        return this;
     }
 }
