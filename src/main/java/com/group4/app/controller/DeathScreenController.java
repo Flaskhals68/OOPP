4c0e822ab32f8422f4050a9d2c4e26878b4821a0
erbergli <erik.berglind@gmail.com>
2023-12-12 14:46:12 +0100

    Update DeathScreenController.java

--- src/main/java/com/group4/app/controller/DeathScreenController.java
+++ src/main/java/com/group4/app/controller/DeathScreenController.java
@@ -1,7 +1,5 @@
 package com.group4.app.controller;
 
-import com.group4.app.App;
-import com.group4.app.view.ActionState;
 
 public class DeathScreenController {
     public DeathScreenController(){
