95ecff2c55e7bb3056ac137183c3c48edbe605f5
erbergli <erik.berglind@gmail.com>
2023-12-12 14:49:02 +0100

    Disable everything when dead

--- src/main/java/com/group4/app/controller/DeathScreenController.java
+++ src/main/java/com/group4/app/controller/DeathScreenController.java
@@ -1,9 +1,10 @@
 package com.group4.app.controller;
 
+import com.group4.app.view.ActionState;
 
 public class DeathScreenController {
     public DeathScreenController(){
-
+        StateController.setState(ActionState.DISABLED);
     }
 
     public void closeGame(){
