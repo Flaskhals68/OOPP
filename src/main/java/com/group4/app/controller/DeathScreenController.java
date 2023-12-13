d057baa6d2625503f481918371ecb8c35c3f88aa
Flaskhals68 <03kajo@gmail.com>
2023-12-13 17:13:11 +0100

    Remove depricated imports

--- src/main/java/com/group4/app/controller/DeathScreenController.java
+++ src/main/java/com/group4/app/controller/DeathScreenController.java
@@ -1,10 +1,7 @@
 package com.group4.app.controller;
 
 import com.group4.app.model.Model;
-import com.group4.app.model.actions.Action;
-import com.group4.app.model.actions.RestartAction;
 import com.group4.app.model.actions.RestartActionInput;
-import com.group4.app.view.ActionState;
 
 public class DeathScreenController {
 
