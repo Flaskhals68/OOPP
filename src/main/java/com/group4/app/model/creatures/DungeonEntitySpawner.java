5578efe8a30479089414bae2d5ba2f8335ff86e6
Noah Andreasson <noahan@student.chalmers.se>
2023-12-15 13:51:20 +0100

    Move DungeonEntitySpawner to creatures and make skeletons spawn

--- src/main/java/com/group4/app/model/creatures/DungeonEntitySpawner.java
+++ src/main/java/com/group4/app/model/creatures/DungeonEntitySpawner.java
@@ -1,11 +1,10 @@
-package com.group4.app.model.dungeon;
-
-import com.group4.app.model.creatures.Creature;
-import com.group4.app.model.creatures.EnemyFactory;
-import com.group4.app.model.creatures.IEnemyManager;
+package com.group4.app.model.creatures;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.group4.app.model.dungeon.Position;
+import com.group4.app.model.dungeon.World;
 
 public class DungeonEntitySpawner {
     public static List<Creature> spawnEnemies(World world, double density, IEnemyManager em) {
@@ -24,9 +23,16 @@ public class DungeonEntitySpawner {
     private static void spawnEnemy(int x, int y, World world, IEnemyManager em, List<Creature> enemies) {
         Position pos = new Position(x, y, world.getId());
         if (!(world.getTile(pos) == null) && world.getEntities(pos).isEmpty()) {
-            Creature e = EnemyFactory.createZombie(pos, em);
-            enemies.add(e);
-            world.add(e);
+            double rand = Math.random();
+            if (rand < 0.75) {
+                Creature e = EnemyFactory.createZombie(pos, em);
+                enemies.add(e);
+                world.add(e);
+            } else {
+                Creature e = EnemyFactory.createSkeleton(pos, em);
+                enemies.add(e);
+                world.add(e);
+            }
         }
     }
 }
